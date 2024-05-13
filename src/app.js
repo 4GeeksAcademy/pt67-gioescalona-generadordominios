a65ef3b739acbce2fcb75d683324e882acc9dc32
Eligio Escalona <158042766+Gioescalona@users.noreply.github.com>
2024-05-13 19:19:09 +0000

    proyecto domain final

--- src/app.js
+++ src/app.js
@@ -6,6 +6,18 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let domains = [];
+
+  for (let i = 0; 1 < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        console.log(pronoun[i] + adj[j] + noun[k]);
+        const domain = pronoun[i] + adj[j] + noun[k] + ".com";
+        domains.push(domain);
+      }
+    }
+  }
 };
